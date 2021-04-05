8631a7bb686f8f586bf7cb5fb2e8a14f17f057a0
Amin Imani <aminpk99@gmail.com>
2021-04-05 20:05:43 +0430

    some fixes

--- src/pages/Container/Container.js
+++ src/pages/Container/Container.js
@@ -2,7 +2,7 @@ import Player from "../../components/Player/Player"
 import {useState} from 'react';
 import './Container.css'
 const Container = () => {
-    const [link, setlink] = useState("https://livenegah01.freecdn.io/hldyqIOcLcXIaUdyFIOcwO5pQnbHwO5p6nbHtWas/V2QwdlRTcnZncU1qVi9qNTBReXFCQT09/li_V2QwdlRTcnZncU1qVi9qNTBReXFCQT09_X5cuKyxNBMLq9S8yTrOr_2021-3-8-15-34-32_,240,360,480,720,.mp4.urlset/master.m3u8")
+    const [link] = useState("https://livenegah01.freecdn.io/hldyqIOcLcXIaUdyFIOcwO5pQnbHwO5p6nbHtWas/V2QwdlRTcnZncU1qVi9qNTBReXFCQT09/li_V2QwdlRTcnZncU1qVi9qNTBReXFCQT09_X5cuKyxNBMLq9S8yTrOr_2021-3-8-15-34-32_,240,360,480,720,.mp4.urlset/master.m3u8")
     return ( 
         <div className="container">
             <Player link ={link}/>
