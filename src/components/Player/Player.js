080da45cf8202e3aae540da4d74f0474d8b19b5a
Amin Imani <aminpk99@gmail.com>
2021-04-05 19:58:06 +0430

    Check quality levels

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,24 +1,26 @@
-import {useEffect,useRef} from 'react';
-import Hls, { HlsUrlParameters } from 'hls.js';
+import {useEffect,useRef,useState} from 'react';
+import Hls from 'hls.js';
 import './Player.css';
 
 
 const Player = ({link}) => {
+    const [qLevels,setQLevels] = useState([])
    const video = useRef();
    useEffect(() => {
-    
-    
     if(Hls.isSupported()){
-        const player = new Hls();
-        player.loadSource(link);
-        player.attachMedia(video.current);
- 
-        console.log(video.current)
-        
+        const hls = new Hls();
+        hls.loadSource(link);
+        hls.attachMedia(video.current);
+   
+        hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+            setQLevels(hls.levels)
+            console.log(hls.levels,'state');
+            if(hls.levels.length!==0){
+                setQLevels(hls.levels)
+                console.log(qLevels);
+            }
+          });
      }
-       return () => {
-           
-       }
    }, [link])
    return ( 
         <div className="video-player">
